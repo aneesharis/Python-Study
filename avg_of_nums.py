ed4e477b42ed98fff1002b394128ba69f9996081
Anees Haris <anees.careers@gmail.com>
2025-01-24 20:07:25 +0530

    Add logic to find avg of numbers

---
+++ avg_of_nums.py
@@ -0,0 +1,10 @@
+def average_of_numbers(numbers):
+    length = len(numbers)
+    return sum(numbers)/length
+
+# Example list of numbers
+numbers = [1, 2, 3, 4, 5]
+
+# Calling the function and printing the result
+result = average_of_numbers(numbers)
+print("The avg of the numbers is:", result)
